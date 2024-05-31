268a0abb0924fd1e11416cad6406964df2296920
neftec99 <neftalitorres@tec.mx>
2024-05-31 17:46:06 -0600

    Mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModules } from './heroes/heroes.module';
+
+
+
+
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesModules
   ],
   providers: [],
   bootstrap: [AppComponent]
